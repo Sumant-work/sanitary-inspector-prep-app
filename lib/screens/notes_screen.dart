18716ab8a09eebd96f995eb5f37f4ce93ab5cdc9
Sumant-work <sumantraj.work@gmail.com>
2025-07-21 04:09:15 +0530

    Create notes_screen.dart

---
+++ lib/screens/notes_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NotesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('📝 Notes')),
+      body: Center(child: Text('Coming Soon: Notes')),
+    );
+  }
+}
