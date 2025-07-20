048b0773a62801b267bb76e9f6ba087803c5f32f
Sumant-work <sumantraj.work@gmail.com>
2025-07-21 04:24:47 +0530

    Update permission_helper.dart

--- lib/utils/permission_helper.dart
+++ lib/utils/permission_helper.dart
@@ -2,7 +2,22 @@ import 'package:permission_handler/permission_handler.dart';
 
 class PermissionHelper {
   static Future<bool> requestStoragePermission() async {
+    if (await Permission.storage.isGranted) return true;
+
     final status = await Permission.storage.request();
-    return status.isGranted;
+    return status == PermissionStatus.granted;
+  }
+
+  static Future<bool> requestMediaPermission() async {
+    if (await Permission.photos.isGranted) return true;
+
+    final status = await Permission.photos.request();
+    return status == PermissionStatus.granted;
+  }
+
+  static Future<bool> checkAllPermissions() async {
+    bool storage = await requestStoragePermission();
+    bool media = await requestMediaPermission();
+    return storage || media;
   }
 }
