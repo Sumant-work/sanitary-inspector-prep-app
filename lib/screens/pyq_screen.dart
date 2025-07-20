e6c448fa5ca6b8c45ad0d2c94fa82d0ad86f08cd
Sumant-work <sumantraj.work@gmail.com>
2025-07-21 04:08:18 +0530

    Create pyq_screen.dart

---
+++ lib/screens/pyq_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PyqScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('📄 PYQ PDFs')),
+      body: Center(child: Text('Coming Soon: PYQ PDFs')),
+    );
+  }
+}
