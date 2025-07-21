5b13da6883712905f93a819c9e7fedeac244dc1a
Sumant-work <sumantraj.work@gmail.com>
2025-07-21 23:18:39 +0530

    Create app_assets.dart

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,24 @@
+class AppAssets {
+  // App Icons
+  static const String appIcon = 'assets/icon/app_icon.png';
+  static const String appIconForeground = 'assets/icon/app_icon_foreground.png';
+  
+  // Images
+  static const String splashImage = 'assets/images/splash_logo.png';
+  static const String homeBackground = 'assets/images/home_bg.png';
+  
+  // Category Icons
+  static const String publicHealthIcon = 'assets/images/icons/public_health.png';
+  static const String waterQualityIcon = 'assets/images/icons/water_quality.png';
+  static const String foodSafetyIcon = 'assets/images/icons/food_safety.png';
+  static const String sanitationIcon = 'assets/images/icons/sanitation.png';
+  
+  // Data Files
+  static const String questionsData = 'assets/data/questions-data.json';
+  static const String notesData = 'assets/data/notes-data.json';
+  static const String categoriesData = 'assets/data/categories-data.json';
+  
+  // Sample PDFs (for testing)
+  static const String samplePyqPdf = 'assets/pdfs/sample_pyq_2023.pdf';
+  static const String sampleNotesPdf = 'assets/pdfs/sample_notes.pdf';
+}
