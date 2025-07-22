5e74ac6e79470f5791f0587648e6158281662966
Sumant-work <sumantraj.work@gmail.com>
2025-07-22 16:46:11 -0700

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/sanitary_inspector_prep_app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.sanitary_inspector_prep_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // No additional code needed for basic V2 embedding
+    // The FlutterActivity handles everything automatically
+    
+    // If you need to add custom functionality, you can override methods here
+    // For example:
+    // override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    //     super.configureFlutterEngine(flutterEngine)
+    //     // Add custom method channels or plugins here
+    // }
+}
